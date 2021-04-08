509b07d622b74b286e25df71a052de39c359826c
Osiris Román <osirisr1994@gmail.com>
2021-04-08 18:03:20 -0500

    Initial endpoint GET

---
+++ controllers/feed.js
@@ -0,0 +1,9 @@
+const getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is the first post!" }],
+  });
+};
+
+module.exports = {
+  getPosts,
+};
