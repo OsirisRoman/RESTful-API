c3d576a6baa5a575ab879bc4cdeb92bc9d3a6e28
Osiris Román <osirisr1994@gmail.com>
2021-04-10 16:16:38 -0500

    Server Side Validation Added.
    The Frontend react app validates the users input when a new post is
    created, but in order to secure the data that will enter into the
    database, it is required the same validation in the backend. This
    validation was achieved using express-validator package.

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 const getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +18,13 @@ const getPosts = (req, res, next) => {
 };
 
 const createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
   //Create post in db
   res.status(201).json({
     message: "Post Created Successfully!",
--- routes/feed.js
+++ routes/feed.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const { body } = require("express-validator");
 
 const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
+const titleValidator = body("title").trim().isLength({ min: 5 });
+const contentValidator = body("content").trim().isLength({ min: 5 });
+
 router.get("/posts", feedController.getPosts);
 
-router.post("/post", feedController.createPost);
+router.post(
+  "/post",
+  [titleValidator, contentValidator],
+  feedController.createPost
+);
 
 module.exports = router;
