c7337ed64106b6da94d7d92c9c7e515e2f585d17
Osiris Román <osirisr1994@gmail.com>
2021-04-14 19:10:20 -0500

    Validating the user JWT at required routes.

--- routes/feed.js
+++ routes/feed.js
@@ -2,28 +2,31 @@ const express = require("express");
 const { body } = require("express-validator");
 
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
 const titleValidator = body("title").trim().isLength({ min: 5 });
 const contentValidator = body("content").trim().isLength({ min: 5 });
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
   "/post",
+  isAuth,
   [titleValidator, contentValidator],
   feedController.createPost
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.put(
   "/post/:postId",
+  isAuth,
   [titleValidator, contentValidator],
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
